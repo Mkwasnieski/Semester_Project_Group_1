21584aa2c79a00228123025cead0ed4c85cec5aa
drschriner <drschriner@unomaha.edu>
2017-03-27 22:13:44 -0500

    Create Contestant API

---
+++ api/controllers/ContestantController.js
@@ -0,0 +1,11 @@
+/**
+ * ContestantController
+ *
+ * @description :: Server-side logic for managing contestants
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
