996a61b6d01e8b1b1ae405e51db1a4fc688d6d2f
drschriner <drschriner@unomaha.edu>
2017-04-02 14:01:59 -0500

    Added Contestant Create Function and Show / New.ejs

--- api/controllers/ContestantController.js
+++ api/controllers/ContestantController.js
@@ -6,6 +6,29 @@
  */
 
 module.exports = {
-	
+
+  'new': function (req, res) {
+    res.view();
+  },
+
+  create: function(req, res, next) {
+      Contestant.create(req.params.all(), function contestantCreated(err,contestant) {
+        if (err) return next(err);
+
+        res.redirect('/contestant/show/' + contestant.id);
+      });
+    },
+
+  show: function(req, res, next) {
+    Contestant.findOne(req.param('id'), function foundContestant(err,contestant) {
+      if (err) return next(err);
+
+      if (!contestant) return next();
+
+      res.view({
+        contestant: contestant
+      });
+    });
+  },
 };
 
