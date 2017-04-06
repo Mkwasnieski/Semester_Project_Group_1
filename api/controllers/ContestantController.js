df2bff59d03c011a364616f7dad10d27d522f922
drschriner <drschriner@unomaha.edu>
2017-04-06 17:00:56 -0500

    Put all the code back in the create function

--- api/controllers/ContestantController.js
+++ api/controllers/ContestantController.js
@@ -13,79 +13,65 @@ module.exports = {
 
   create: function (req, res, next) {
 
-    function randomInt() {
-      var high = 88;
-      var low = 1;
-      return Math.floor(Math.random() * (high - low) + low);
-    };
+    Contestant.create(req.params.all(), function contestantCreated(err, contestant) {
+      if (err) return next(err);
 
-    var swname = "";
-    /// response, player, and callback
-    function process_response(webservice_response, callback) {
-      var webservice_data = "";
-      webservice_response.on('error', function (e) {
-        console.log(e.message);
-        callback("Error: " + e.message);
-      });
-
-      webservice_response.on('data', function (chunk) {
-        webservice_data += chunk;
-      });
-
-      webservice_response.on('end', function () {
-        starwars_data = JSON.parse(webservice_data);
-        swname = starwars_data.name;
-        //player.starwars_name = starwars_data.name;
-        //player.save()
-        //pass contestant in both
-        callback()
-      });
-    }
-
-    //call contestant player here
-    function get_starwars_name(contestant, callback) {
-
-      var http = require('http');
-
-      options = {
-        host: 'swapi.co',
-        port: 80,
-        path: '/api/people/' + randomInt() + "/",
-        method: 'GET'
+      function randomInt() {
+        var high = 88;
+        var low = 1;
+        return Math.floor(Math.random() * (high - low) + low);
       };
 
-      var webservice_request = http.request(options, function(response) {
-        //resposne, players, callback
-        process_response(response, callback)
+      function process_response(webservice_response, player, callback) {
+        var webservice_data = "";
+        webservice_response.on('error', function (e) {
+          console.log(e.message);
+          callback("Error: " + e.message);
+        });
+
+        webservice_response.on('data', function (chunk) {
+          webservice_data += chunk;
+        });
+
+        webservice_response.on('end', function () {
+          starwars_data = JSON.parse(webservice_data);
+          player.starwars_name = starwars_data.name;
+          player.save(
+           function (player) {
+              console.log(player.starwars_name);
+           });
+        });
+          callback();
+      }
+
+      function get_starwars_name(player, callback) {
+
+        var http = require('http');
+
+        options = {
+          host: 'swapi.co',
+          port: 80,
+          path: '/api/people/' + randomInt() + "/",
+          method: 'GET'
+        };
+
+        var webservice_request = http.request(options, function (response) {
+          process_response(response, player, callback)
+        });
+        webservice_request.end();
+      };
+
+      async.each(contestant, get_starwars_name, function (err) {
+        if (err) console.log(err);
+
+        res.view({
+          contestant: contestant
+        });
       });
-      webservice_request.end();
 
-      //console.log(starwars_name);
-    };
-
-    swname = get_starwars_name();
-
-
-    async.each(contestant, get_starwars_name, function(err) {
-      parms = req.params.all();
-      parms.starwars_name = swname;
-
-      Contestant.create(parms, function contestantCreated(err, contestant) {
-        if (err) return next(err);
-
-        res.redirect('/contestant/show/' + contestant.id);
-      });
-    },
-
-      if(err) console.log(err);
-      //console.log('done');
-
-      res.view({
-        contestant: contestant
-      });
+      res.redirect('/contestant/show/' + contestant.id);
     });
-
-
+  },
 
   show: function (req, res, next) {
     Contestant.findOne(req.param('id'), function foundContestant(err, contestant) {
@@ -98,5 +84,6 @@ module.exports = {
       });
     });
   },
-};
+
+}
 
